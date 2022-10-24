c25f08200f226025cb5b0ed00e3280766f6f82b1
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-24 09:24:29 +0500

    06

--- basic_func06.py
+++ basic_func06.py
@@ -1,3 +1,10 @@
 # Create a function called main.
 
 # Return the value str type.
+#from multiprocessing.connection import answer_challenge
+#x^{2}+6x^{3}+3xy
+
+x = 5 
+y = 2
+answer=(pow(x,2)+6*pow(x,3)+3*x*y)
+print(answer)
