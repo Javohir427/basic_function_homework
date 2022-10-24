a7e8c7f36ccc5bc11940e3d478d908dffda2c37e
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-24 10:27:37 +0500

    06

--- basic_func06.py
+++ basic_func06.py
@@ -3,8 +3,10 @@
 # Return the value str type.
 #from multiprocessing.connection import answer_challenge
 #x^{2}+6x^{3}+3xy
+#from os import RWF_HIPRI
 
-x = 5 
-y = 2
-answer=(pow(x,2)+6*pow(x,3)+3*x*y)
-print(answer)
+
+def main(x):
+    return(str(x))
+
+print(main(4+3))
